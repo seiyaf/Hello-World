9b0fcf3658b5c8f01fd69098470b428078300a6b
seiyaf <seiyafukusima@gmail.com>
2021-01-19 05:45:05 +0900

    2021/1/19　eclipseのテスト

---
+++ Hello/src/header.h
@@ -0,0 +1,13 @@
+/*
+ * header.h
+ *
+ *  Created on: 2021/01/16
+ *      Author: seiya
+ */
+
+#ifndef HEADER_H_
+#define HEADER_H_
+
+
+
+#endif /* HEADER_H_ */
---
+++ Hello/src/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: 2021/01/16
+ *      Author: seiya
+ */
+
+#include <stdio.h>
+
+int main(){
+	printf("Hello World.\n");
+
+	return 0;
+}
