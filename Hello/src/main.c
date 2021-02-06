cd71e69e1699bce6cde3460fc1914f7f1b37935d
seiyaf <seiyafukusima@gmail.com>
2021-02-06 09:55:39 +0900

    2021/02/06
    Egit連携したeclipseからの更新テスト

--- Hello/src/main.c
+++ Hello/src/main.c
@@ -13,28 +13,6 @@
 void dobleup(int*);
 
 int main(){
-	/*printf("Hello World.\n");
-
-	int ar1[SIZE];
-	char ar2[SIZE];
-	int i;
-	int* p1 = NULL;
-	char* p2 = NULL;
-	//  値を代入
-	for(i = 0; i < SIZE; i++){
-		ar1[i] = i;
-		ar2[i] = 'A'+i;
-	}
-	//  ポインタにアドレスを代入
-	p1 = &ar1[0];
-	p2 = &ar2[0];
-	//  値を出力
-	for(i = 0; i < SIZE; i++){
-		printf("ar1[%d]=%d *(p1+%d)=%d ",i,ar1[i],i,*(p1+i));
-		printf("ar2[%d]=%c *(p2+%d)=%c¥n",i,ar2[i],i,*(p2+i));
-	}
-
-	return 0;*/
 
     //  サイズSIZEの配列を用意する。
     int nums[SIZE]  ;
